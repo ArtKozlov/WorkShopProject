ed99f73047e54e9ee6dcce4191b68f949843df52
Artyom <Artsiomxxx@gmail.com>
2017-01-30 23:23:53 +0300

    Fix create operation(ESL). Work. Need to do refactoring.

---
+++ todoclient/ElasticSearch/Indices/ItemIdx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticSearch.Indices
+{
+    public class ItemIdx
+    {
+       
+        public int Id { get; set; }
+        public int ToDoId { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+        public int UserId { get; set; }
+    }
+}
--- todoclient/ElasticSearch/Queries/ItemQueries.cs
+++ todoclient/ElasticSearch/Queries/ItemQueries.cs
@@ -1,9 +1,6 @@
 ﻿using ElasticSearch.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ElasticSearch.Indices;
 using Nest;
 
@@ -19,49 +16,13 @@ namespace ElasticSearch.Queries
         public void Create(Item e)
         {
             var local = new Uri("http://localhost:9200");
-            var settings = new ConnectionSettings(local).DefaultIndex("Items"); ;
+
+            var settings = new ConnectionSettings(local)
+                .DefaultIndex("toDoList");
+            var resolver = new IndexNameResolver(settings);
+            var index = resolver.Resolve<Item>();
             var client = new ElasticClient(settings);
-
-            var descriptor = new CreateIndexDescriptor("Item")
-                .Mappings(ms => ms
-                    .Map<Item>(m => m.AutoMap()));
-
-            var expected = new
-            {
-                mappings = new
-                {
-                    item = new
-                    {
-                        properties = new
-                        {
-                            id = new
-                            {
-                                type = "integer"
-                            },
-                            toDoId = new
-                            {
-                                type = "integer"
-                            },
-                            name = new
-                            {
-                                type = "text"
-                            },
-                            isCompleted = new
-                            {
-                                type = "boolean"
-                            },
-                            userId = new
-                            {
-                                type = "integer"
-                            }
-                        }
-                    }
-                }
-
-            };
-
-
-            client.Index(expected);
+            client.Index(index);
             client.Create(e);
         }
 
